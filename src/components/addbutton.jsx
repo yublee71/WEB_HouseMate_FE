108ed6b6d9b4897bcc774f4c49a0dba9236f4bd9
yublee71 <lybin71@naver.com>
2025-02-07 22:27:18 +0000

    feat: add addsection - ongoing

--- src/components/addbutton.jsx
+++ src/components/addbutton.jsx
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import { AddSection } from "./addsection";
+import { useState } from "react";
 
 const StyledAddButton = styled.button`
   position: fixed;
@@ -12,13 +14,39 @@ const StyledAddButton = styled.button`
   padding: 2px 0 0 2px;
   img {
     height: 30%;
+    &:hover {
+      cursor: pointer;
+    }
   }
 `;
 
+// const StyledAddSection = styled.section`
+//   position: fixed;
+//   top: 20%;
+//   left: 15%;
+//   height: 60%;
+//   width: 70%;
+//   background-color: var(--color-grey-100);
+//   border-radius: 15px;
+//   box-shadow: 0px 1px 9px rgba(128, 128, 128, 0.404);
+// `;
+
 export function AddButton() {
+  const [isVisible, setVisibility] = useState(false);
   return (
-    <StyledAddButton>
-      <img src="/src/assets/addsign.png" alt="add sign" height="24px" />
-    </StyledAddButton>
+    <>
+      <AddSection></AddSection>
+      <StyledAddButton>
+        <img
+          src="/src/assets/addsign.png"
+          alt="add sign"
+          height="24px"
+          onClick={() => {
+            setVisibility(true);
+          }}
+        />
+        {/* {isVisible && <StyledAddSection>hi</StyledAddSection>} */}
+      </StyledAddButton>
+    </>
   );
 }
---
+++ src/components/addsection.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const StyledAddSection = styled.section`
+  position: fixed;
+  top: 20%;
+  left: 20%;
+  height: 60px;
+  width: 70px;
+  background-color: var(--color-grey-100);
+  border-radius: 15px;
+  box-shadow: 0px 1px 9px rgba(128, 128, 128, 0.404);
+`;
+
+export function AddSection() {
+  <StyledAddSection>hi</StyledAddSection>;
+}
